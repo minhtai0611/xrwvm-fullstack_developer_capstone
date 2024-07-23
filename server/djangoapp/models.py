111df217b9314868811a5ae8fb831625470d6553
minhtai0611 <minhtainth06111999@gmail.com>
2024-07-22 23:24:19 -0400

    commit

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -17,7 +17,7 @@ class CarMake(models.Model):
 # Car Model model
 class CarModel(models.Model):
     car_make = models.ForeignKey(
-        CarMake, 
+        CarMake,
         on_delete=models.CASCADE
     )  # Many-to-One relationship
     name = models.CharField(max_length=100)
